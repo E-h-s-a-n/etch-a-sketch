165f257bdd0be385338edf1bd057d1dd45d0839e
Ehsan <ehsan440@rocketmail.com>
2022-10-03 23:44:23 +0330

    Add single and random color

--- script.js
+++ script.js
@@ -13,37 +13,12 @@ const RGB_Linear_Shade=(p,c)=>{
         (d?","+d:")");
 }
 
-function setActiveMode(ev){
-    modeBtn.forEach((el)=>{
-        el.classList.remove('active-mode');
-    });
-    this.classList.add('active-mode');
-    // colorMode = this.
-
+const HSLtoRGB=(hsl)=>{
+    return +hsl;
 }
 
-function newPanel(maxTile = 16){
-    tileGrid.style.gridTemplateColumns = `repeat(${maxTile}, 1fr)`;
-    tileGrid.style.gridAutoRows = `${700/maxTile}px`;
-    panelSize.textContent = `${maxTile}x${maxTile}`
-    for (let i = 0; i < maxTile**2 + 0; i++) {
-        const tile = document.createElement('div');
-        tile.addEventListener('mouseenter', setTileColor);
-        tileGrid.appendChild(tile);
-    }
-}
-
-const colorMode = 'random'
-function setTileColor(ev){
-    if (ev.buttons > 0){
-        const r = getRandomInt
-        let color
-        if (colorMode == 'random') color = `hsl(${r(360)}deg, ${r(60, 70)}%, ${r(35, 75)}%)`;
-        if (colorMode == 'single') color = 'rgb(100, 100, 100)'
-        // const color = RGB_Linear_Shade(-0.1, this.style.backgroundColor)
-        // this.style.backgroundColor = color;
-        this.style.backgroundColor = color;
-    }
+const HEXtoRGB=(hex)=>{
+    return +hex;
 }
 
 function getRandomInt(min, max){
@@ -52,16 +27,69 @@ function getRandomInt(min, max){
     return Math.floor(Math.random()*(max-min+1)+min);
 }
 
+// modeBtn click function
+function setActiveMode(){
+    modeBtn.forEach((el)=>{
+        el.classList.remove('active-mode');
+    });
+    this.classList.add('active-mode');
+    colorMode = this.getAttribute('data-mode');
+}
+
+function createPanel(maxTile = 16){
+    tileGrid.style.gridTemplateColumns = `repeat(${maxTile}, 1fr)`;
+    tileGrid.style.gridAutoRows = `${700/maxTile}px`;
+    panelSizeSpan.textContent = `${maxTile}x${maxTile}`;
+    for (let i = 0; i < maxTile**2 + 0; i++) {
+        const tile = document.createElement('div');
+        tile.classList.add('div-borders');
+        tile.addEventListener('mouseenter', setTileColor, false);
+        tileGrid.appendChild(tile);
+    }
+}
+
+// tile mouseenter function
+function setTileColor(ev){
+    if (!ev.buttons>0) return;
+
+    const r = getRandomInt;
+    let color;
+    if (colorMode == 'random') color = `hsl(${r(360)}deg, ${r(60, 70)}%, ${r(35, 75)}%)`;
+    if (colorMode == 'single') color = singleColor;
+    // const color = RGB_Linear_Shade(-0.1, this.style.backgroundColor)
+    // this.style.backgroundColor = color;
+    this.style.backgroundColor = color;
+    
+}
+
+const colorWell = (e)=>{
+    singleColor = e.target.value;
+    console.log(`color change= ${singleColor}`)
+}
+
+function newPanel(){
+    let n = prompt('max tile number', 16);
+    n = (+n) ? +n : 16;
+    n = n>90?90:n<2?2:n;
+    console.log('new panel', n);
+    tileGrid.replaceChildren()
+    createPanel(n);
+}
+
 
 const tileGrid = document.querySelector('.container');
 const modeBtn = document.querySelectorAll('button.mode');
 const randomSelect = document.querySelector('#random');
-const panelSize = document.querySelector('h3 span');
+const panelSizeSpan = document.querySelector('h3 span');
 
+let colorMode = 'random';
+let singleColor = 'rgb(150, 150, 150)'
+
+document.querySelector('#colorWell').addEventListener('change', colorWell);
+document.querySelector('.new').addEventListener('click', newPanel)
 
 modeBtn.forEach((el,)=>{
-    console.log('done');
     el.addEventListener('click', setActiveMode);
 });
 
-newPanel(8);
+createPanel(16);
